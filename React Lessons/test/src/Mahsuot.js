3a3f708a98985b0c26d9f44faac16a67b241a8dd
Ulugbek01 <xyz207820@gmail.com>
2022-02-17 13:25:39 +0500

    add and delete function add

--- React Lessons/test/src/Mahsuot.js
+++ React Lessons/test/src/Mahsuot.js
@@ -1,16 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MahsuotlarContext } from "./context";
 
 export const Mahsuot = () => {
   const [data, setData] = useContext(MahsuotlarContext);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
 
   const onDelete = (id) => {
     let newData = data.filter((value) => value.id !== id);
     setData(newData);
   };
 
+  const getName = ({ target }) => {
+    setName(target.value);
+  };
+
+  const getSurname = ({ target }) => {
+    setSurname(target.value);
+  };
+
+  const onAdd = () => {
+    // console.log(Boolean(name && surname));
+    if (name && surname) {
+      let newData = {
+        id: data.length + 1,
+        title: name,
+        path: surname,
+      };
+      setData([...data, newData]);
+    } else {
+      alert("Janob iltimos malumot kiriting");
+    }
+  };
+
   return (
     <div>
+      <input placeholder="name" onChange={getName} />
+      <input placeholder="surname" onChange={getSurname} />
+      <button onClick={onAdd}>add</button>
+      <h4>Name: {name}</h4>
+      <h4>Surname: {surname}</h4>
       {data.map((value, index) => (
         <div
           key={value.id}
